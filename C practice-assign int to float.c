f09e30069049547cbe6fbe95d999d72900f8b7d1
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-09 20:26:16 +0530

    Add files via upload

---
+++ C practice-assign int to float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int value = 10;
+    float f = value;
+    printf("%.1f \n",f);
+    int a = 25;
+    float f1 = a;
+    printf("%.1f",f1);
+    return 0;
+}
