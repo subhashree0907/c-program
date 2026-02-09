de9f15bd5b5dea6919d7548434da1e0c6b698703
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-09 21:24:55 +0530

    Add files via upload

---
+++ C practice-print Quotient and remainder of two integers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("Quotient=%d ",a/b);
+    printf("remainder=%d",a%b);
+    return 0;
+} 
