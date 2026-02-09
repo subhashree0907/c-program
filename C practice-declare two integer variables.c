4bae286a1999ce79c87edc2dc343f6a26a5e2067
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-09 15:22:24 +0530

    Add files via upload

---
+++ C practice-declare two integer variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5;
+    printf("%d ",a);
+    int b=15;
+    printf("%d \n",b);
+    int c=20;
+    printf("%d ",c);
+    int d=40;
+    printf("%d",d);
+    return 0;
+}
