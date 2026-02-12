286ef8c853583133e6b78ff1172c1b460589e72c
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-12 10:46:58 +0530

    Add files via upload

---
+++ C practice-check even or odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   scanf("%d", &a);
+   if (a %2==0){
+       printf("Even number");
+   }
+   else{
+       printf("Odd number");
+   }
+   return 0;
+}
