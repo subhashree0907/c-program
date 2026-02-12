258906d9af2e457450fdc11c3f65590586a35099
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-12 11:02:54 +0530

    Add files via upload

---
+++ C practice-find greater of two number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a = 10;
+   int b = 20;
+   if (b>a){
+       printf("20 is greater");
+   }
+   else{
+       printf("10 is greater");
+   }
+   return 0;
+}
