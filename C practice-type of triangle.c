c50f7de072b6f3bc962ff0d8cadd44b7e479face
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-12 16:25:48 +0530

    Add files via upload

---
+++ C practice-type of triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int a, b, c;
+   scanf("%d %d %d", &a, &b, &c);
+   
+   if (a==b && b==c){
+       printf("Equilateral triangle");
+   }
+   else {
+       printf("Not Equilateral triangle");
+   }
+   return 0;
+}
