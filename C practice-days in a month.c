891ed5f21a7ffb1a645d3aa42b343844f2ce0656
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-13 15:46:37 +0530

    Add files via upload

---
+++ C practice-days in a month.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main()
+{
+    int month;
+    scanf("%d", &month);
+    
+    switch(month){
+        case 1:
+            printf("31 days \n");
+        break;
+        
+        case 2:
+            printf("28 or 29 days \n");
+        break;
+        
+        case 3:
+            printf("30 days \n");
+        break;
+        
+        case 4:
+            printf("31 days \n");
+        break;
+        
+        case 5:
+            printf("30 days \n");
+        break;
+        
+        case 6:
+            printf("31 days \n");
+        break;
+        
+        case 7:
+            printf("30 days \n");
+        break;
+        
+        case 8:
+            printf("31 days \n");
+        break;
+        
+        case 9:
+            printf("30 days \n");
+        break;
+        
+        case 10:
+            printf("31 days \n");
+        break;
+        
+        case 11:
+            printf("30 days \n");
+        break;
+        
+        case 12:
+            printf("31 days \n");
+        break;
+        
+    default:
+        printf("invaled input");
+         
+    }
+    return 0;
+}
