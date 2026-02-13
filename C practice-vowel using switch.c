8c43bd80c3ba0c060792f98177c8bd75500731a1
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-13 16:02:07 +0530

    Add files via upload

---
+++ C practice-vowel using switch.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    
+    switch(ch){
+        case 'A':
+            printf("Vowel");
+        break;
+        
+        case 'E':
+            printf("Vowel");
+        break;
+        
+        case 'I':
+            printf("Vowel");
+        break;
+        
+        case 'O':
+            printf("Vowel");
+        break;
+        
+        case 'U':
+            printf("Vowel");
+        break;
+        
+        case 'a':
+            printf("Vowel");
+        break;
+        
+        case 'e':
+            printf("Vowel");
+        break;
+        
+        case 'i':
+            printf("Vowel");
+        break;
+        
+        case 'o':
+            printf("Vowel");
+        break;
+        
+        case 'u':
+            printf("Vowel");
+        break;
+        
+        
+    default:
+        printf("invaled input");
+         
+    }
+    return 0;
+}
