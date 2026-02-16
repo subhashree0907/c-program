45926dfcd737bb531a025af6372c3e574e03a93f
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-02-16 10:00:09 +0530

    Add files via upload

---
+++ C practice-Menu Driven-program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int ch,a,b;
+    scanf("%d %d %d",&ch,&a,&b);
+    if(ch==1)
+    {
+    switch(ch)
+    {
+    case 1:
+        printf("Sum =%d",a+b);
+        break;
+        }
+    }
+
+    return 0;
+}
