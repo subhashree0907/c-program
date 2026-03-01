a9618d98acc17877f866d8f0dedcc27eda006a54
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-01 18:24:38 +0530

    Add files via upload

---
+++ C Practice-Smart Traffic Signal Congestion Controller.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int N,vehicles;
+    int i=0;
+    int congestionMinutes=0;
+    int currentStreak=0;
+    int longestStreak=0;
+    scanf("%d",&N);
+    while(i<N) 
+    {
+        scanf("%d",&vehicles);
+
+        if(vehicles>20)
+        {
+            congestionMinutes++;
+            currentStreak++;
+            if(currentStreak>longestStreak)
+                longestStreak=currentStreak;
+        } else 
+        {
+            currentStreak=0;
+        }
+        i++;
+    }
+    printf("Congestion Minutes:%d\n",congestionMinutes);
+    printf("Longest Congestion Streak:%d",longestStreak);
+    return 0;
+}
