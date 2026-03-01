e445c623fa9978a066e59d00006039c96383353a
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-01 18:20:29 +0530

    Add files via upload

---
+++ C Practice-Warehouse Shipment Delay Detector.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int delay;
+    int i=0;
+    int totalDelay=0;
+    int delayedDays=0;
+    scanf("%d",&N);
+    while (i<N)
+    {
+        scanf("%d",&delay);
+        totalDelay=totalDelay+delay;
+        if(delay>2) 
+        {
+            delayedDays++;
+        }
+        i++;
+    }
+    printf("Total Delay:%d \n",totalDelay);
+    printf("Delayed Days:%d",delayedDays);
+    return 0;
+}
