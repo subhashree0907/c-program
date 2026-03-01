86cb6c4eab3e418ccc0c9b36211cd93426ef64a4
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-01 18:22:26 +0530

    Add files via upload

---
+++ C Practice-Employee Overtime Risk Analyzer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int N,hours;
+    int i=0;
+    int totalOvertime=0;
+    int burnoutDays=0;
+    scanf("%d",&N);
+    while(i<N) 
+    {
+        scanf("%d",&hours);
+        totalOvertime+=hours;   
+        if(hours>4)
+        {         
+            burnoutDays++;
+        }
+        i++;
+    }
+    printf("Total Overtime:%d\n",totalOvertime);
+    printf("Burnout Days:%d",burnoutDays);
+    return 0;
+}
