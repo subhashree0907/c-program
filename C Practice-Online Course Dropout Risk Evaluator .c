c79c5026774ea90eaf4c1d2fee0b8dafbda851aa
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-01 18:20:04 +0530

    Add files via upload

---
+++ C Practice-Online Course Dropout Risk Evaluator .c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int hours;
+    int i=0;
+    int inactiveWeeks=0;
+    scanf("%d",&N);
+    while(i<N) 
+    {
+        scanf("%d",&hours);
+        if(hours==0) 
+        {
+            inactiveWeeks++;
+        }
+        i++;
+    }
+    printf("Inactive Weeks:%d \n",inactiveWeeks);
+    if(inactiveWeeks>=3) 
+    {
+        printf("Risk Status:High");
+    } else 
+    {
+        printf("Risk Status:Low");
+    }
+    return 0;
+}
