cb913330bc120b35c6c7acbb706efe9d133c6f28
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-02 13:53:41 +0530

    Add files via upload

---
+++ C Practice-Double inverted full pyramid star pattern.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    int n=5;
+    for(i=0;i<2*n;i++)
+        printf("*");
+    printf("\n");
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        for(j=1;j<=2*(n-i);j++)
+            printf(" ");
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    for(i=2;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        for(j=1;j<=2*(n-i);j++)
+            printf(" ");
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    for(i=0;i<2*n;i++)
+        printf("*");
+    printf("\n");
+    return 0;
+}
