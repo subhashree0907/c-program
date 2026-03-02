0a56596dd56435ef68ed53ddbcee0a93fac74b90
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-02 13:46:52 +0530

    Add files via upload

---
+++ C Practice-Alphabet Palindrome Pyramid Pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,n=4; 
+    char ch;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<i;j++
+        ) {
+            printf("%c",'A'+j);
+        }
+        for(j=i-2;j>=0;j--)
+        {
+            printf("%c",'A'+j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
