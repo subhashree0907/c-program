b59ff41aa731fc6830bb9a3ef2d80fdf6a968e44
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-02 13:53:19 +0530

    Add files via upload

---
+++ C Practice-Nested Loop pattern.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,rows=5;
+    for(i=1;i<=rows;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        for(j=1;j<=2*(rows-i);j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=rows-1;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        
+        {
+            printf("*");
+        }
+        for(j=1;j<=2*(rows-i);j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
