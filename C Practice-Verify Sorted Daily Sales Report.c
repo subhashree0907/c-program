6344eec6df059c229139ccb2952c4805370b7933
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-03 16:29:18 +0530

    Add files via upload

---
+++ C Practice-Verify Sorted Daily Sales Report.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            printf("No");
+            return 0;
+        }
+    }
+    printf("Yes");
+    return 0;
+}
