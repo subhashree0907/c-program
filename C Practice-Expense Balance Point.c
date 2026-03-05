d759823ebf395a99bf5231eab4057ab14c6058f4
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-05 15:10:55 +0530

    Add files via upload

---
+++ C Practice-Expense Balance Point.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int total=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        total+=arr[i];
+    }
+    int leftSum=0;
+    for(int i=0;i<n;i++)
+    {
+        total-=arr[i];   
+        if(leftSum==total)
+        {
+            printf("%d",i);
+            return 0;
+        }
+        leftSum+=arr[i];
+    }
+    printf("-1");
+    return 0;
+}
