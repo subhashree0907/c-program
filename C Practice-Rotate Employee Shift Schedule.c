13defdb6e200ea478d4e297a21b56357246d8813
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-05 15:09:27 +0530

    Add files via upload

---
+++ C Practice-Rotate Employee Shift Schedule.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,i;
+    scanf("%d",&n);
+    int arr[100];
+    for(i=0; i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=k;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    for(i=0;i<k;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
