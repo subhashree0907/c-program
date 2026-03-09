5abcbacc363040f08182372313a0f23164ab4f97
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-09 09:33:05 +0530

    Add files via upload

---
+++ C Practice-Count Peak Traffic Hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=1;i<n-1;i++)
+    {
+        if(a[i]>a[i-1] && a[i]>a[i+1])
+            count++;
+    }
+    printf("%d",count);
+    return 0;
+}
