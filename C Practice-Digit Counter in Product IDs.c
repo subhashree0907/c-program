1ecd745ce8aba4bb58a5d4481009cb52daf106af
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-16 15:44:24 +0530

    Add files via upload

---
+++ C Practice-Digit Counter in Product IDs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    int i=0,count=0;
+    scanf("%s",str);
+    while (str[i]!='\0')
+    {
+        if (str[i]>='0' && str[i]<='9')
+        {
+            count++;
+        }
+        i++;
+    }
+    printf("%d",count);
+    return 0;
+}
