b6feecbeeb0090ae709bbf758adbdfa6f8ec3422
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-16 09:50:34 +0530

    Add files via upload

---
+++ C practice-Fibonacci Series Using Function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void fibonacci(int n) {
+    int a = 0, b = 1, next;
+    
+    for (int i=1; i<=n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    fibonacci(N);
+    
+    return 0;
+}
