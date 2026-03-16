b1dbbafd9360afdd364aa6d3b3fac7f4ce8893bb
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-16 15:47:09 +0530

    Add files via upload

---
+++ C Practice-Manual String Copy (Memory Backup Tool).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str1[100],str2[100];
+    int i=0;
+    scanf("%s",str1);
+    while(str1[i]!='\0')
+    {
+        str2[i]=str1[i];
+        i++;
+    }
+    str2[i]='\0';
+    printf("%s",str2);
+    return 0;
+}
