9653b8438634574e21639e3d631c38b471b2996a
Subha Shree N <subhashreenagaraj0907@gmail.com>
2026-03-16 09:36:08 +0530

    Add files via upload

---
+++ C practice-Find Sum of Digits Using Function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int sumOfDigits(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int result = sumOfDigits(N);
+    printf("%d\n", result);
+
+    return 0;
+}
